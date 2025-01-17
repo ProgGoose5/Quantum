c22294c5ae9eab9aa27945bc2deee00944f6dcbb
Goosely! <wwgoose5@gmail.com>
2025-01-17 04:08:06 +0000

    Adding an Struct to Print boxes around the files
    Still working...

--- globalfunc.c
+++ globalfunc.c
@@ -7,14 +7,19 @@
 #define PAIR2 2
 #define PAIR3 3
 //Int Variables
-int x, y, quantity, gap, selectedfile=0, actualfile=0, filelayout, cou;
+int x, y, i, quantity, gap, selectedfile=0, actualfile=0, filelayout, cou;
 int NumDir= 0;
 int c= 0;
 //CharStrings 
 char* directory[]={"","/home/goose"};
 //Char Variables
 char* cdcom= "cd ";
-char* options[1000] = {NULL};
+char* options[1000] = {" "};
+
+struct Fileubs
+{
+  int xposition, yposition, xlength, ylength, actualy;
+}Fileubs1,Fileubs2,Fileubs3,Fileubs4,Fileubs5;
 
 
 void definitions()
@@ -60,18 +65,61 @@ if(lns != LINES || cols != COLS){
   y = lns-1;
 }
 }
+void boxesdef() {
 
+int txright= (x/4)*3;
+int boxesdivisions= txright/5;
+int initboxes= 29;
+//definition of Fileubs1
+Fileubs1.xposition= initboxes;
+Fileubs1.yposition= 1;
+Fileubs1.ylength= y/5;
+Fileubs1.actualy= 0;
+Fileubs1.xlength= boxesdivisions;
+
+//definition of Fileubs2
+Fileubs2.xposition= ((x/4)/5) + ((x/4)/5);
+Fileubs2.yposition= y/5;
+Fileubs2.actualy= 0;
+Fileubs2.xlength= (int)strlen(options[Fileubs2.actualy]);
+
+//definition of Fileubs3
+Fileubs3.xposition= (Fileubs2.xposition) + ((x/4)/5);
+Fileubs3.yposition= y/5;
+Fileubs3.actualy= 0;
+Fileubs3.xlength= (int)strlen(options[Fileubs3.actualy]);
+
+//definition of Fileubs4
+Fileubs4.xposition= (Fileubs3.xposition) + ((x/4)/5);
+Fileubs4.yposition= y/5;
+Fileubs4.actualy= 0;
+Fileubs4.xlength= (int)strlen(options[Fileubs4.actualy]);
+
+//definition of Fileubs5
+Fileubs5.xposition= (Fileubs4.xposition) + ((x/4)/5);
+Fileubs5.yposition= y/5;
+Fileubs5.actualy= 0;
+Fileubs5.xlength= (int)strlen(options[Fileubs5.actualy]);
+
+
+}
 void Boxrep(){
-    int x2= x/4+1;
-    int appdiv= x2/5;
+  for(int G= 0; G<5; G++){
+    for(int L=Fileubs1.yposition; L<=Fileubs1.ylength; L++){
+      if(actualfile==i){
+        attron(COLOR_PAIR(PAIR2));
+      }
+      else{
+        attron(COLOR_PAIR(PAIR1));
+      }
+      for(int O=(Fileubs1.xposition); O<(Fileubs1.xlength+29); O++)
+      {
+        mvprintw(L, O, " ");
+      }  
+      
+    }    
 
-    for(int l=0; l<4; l++){
-      move(l+cou+2,x2);
-        for(int k= 0; k<appdiv; k++){
-            
-            printw("   ");
-        }
-    }
+}
 }
 
 void callsystem(){
@@ -96,34 +144,30 @@ void callsystem(){
     index++;
     quantity=index;
   }
-
   pclose(Readings);
+
   actualfile=0;
   c=(x/4)+1;
   cou=2; 
-  for(int i=0; i < index; i++){
+  for(i=0; i < index; i++){
     
     if (i == selectedfile)
     { attron(COLOR_PAIR(PAIR2));}
     else
     { attron(COLOR_PAIR(PAIR1));}
-    
-    Boxrep();
-
+   
 
     if((i%5)==0)
     { cou+=4; c=(x/4)+1;}
-    gap=(sizeof(options[i]))/4;
+    gap=5;
 
-    
-    c+= gap;
+    //c+= gap;
     mvprintw(cou, c, "%s", options[i]);
-    c+= sizeof(options[i])+gap;
+    c+= strlen(options[i])+gap;
   }
 
 
   attron(COLOR_PAIR(PAIR1));
-  //mvprintw(8,8,"%d", actualfile);
   refresh();
   }
 
@@ -142,6 +186,11 @@ void KeyCommands(){
 
 
   }
+
+}
+
+
+#endif
 
 }
 
