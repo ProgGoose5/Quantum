aa66621998d7dce8a335fd5a88195d6415e2304d
Goosely! <wwgoose5@gmail.com>
2025-01-24 02:32:07 +0000

    Commenting on main.c

--- main.c
+++ main.c
@@ -3,26 +3,32 @@
 
 int main()
 {
+    //Define inmutable variables.
     definitions();  
-    
     
     clear();
     
      while(1){
-        
-        genprint();
-        Resizing(); 
-        blackout();
-        boxesdef(); 
-        Boxrep();
-        genprint();        
-        //callsystem(); 
-        KeyCommands();
-        
-        
 
+    //Calling resizing for measure the terminal.
+        Resizing(); 
+    //Defining properties of the boxes
+        boxesdef();
+    //Refresh important variables...
+        alwaysrefresh();
+    //Callin' a read of the keyboard.
+        KeyCommands();
+    //Call a blackout to clean the screen 
+        blackout(); 
+    //Redifine the boxes (Just in case...)
+        boxesdef();         
+    //Call the box printing(where the boxes print vertically)
+        Boxrep();
+    //Call the General Printing (For the appeareance.)
+        genprint();        
+    //Ask the system to get the directories 
+        callsystem(); 
+    //Repeat in loop.
     } 
-   return 0;
-}
    return 0;
 }
