2c73d8de2acc515a325d5337330b5d41533e7fd6
Goosely! <wwgoose5@gmail.com>
2025-01-31 01:26:36 +0000

    Adding the new functions...
    Added:
    +Creating Files
    +Copy, Cut and Paste Files
    +Rename Files
    +Execute Locally
    +Execute with Wine

--- actions.c
+++ actions.c
@@ -1,19 +1,24 @@
 #include "librariesreq.h"
 #ifndef ACTIONS_C
-
-
 #include "globalfunc.c"
 
-char *commands[10]={"mkdir ", "touch ", "cp ", "mv ", "cp ", "rename", "exec", "wine", "cmake ", "make "};
+
+char *commands[10]={"mkdir ", "touch ", "cp ", "mv ", "cp ", "mv ", "./", "wine ", "cmake ", "make "};
+char copyfrom[256];
+bool ccopy= FALSE;
+bool ccut= FALSE;
 
 void Createdir(){
     char makeadir[256] = "";
     char newdir[10]= "/";
     char cndir[256];
+    memset(makeadir, 0, sizeof(makeadir));
     strcat(makeadir, commands[0]);
     strcat(makeadir, combinedDir);
-    mvprintw(1, (x/4+1), "New Directory Name:");
+    mvprintw(1, (x/4+1), "New Directory Name: ");
+    echo();
     getstr(cndir);
+    noecho();
     refresh();
     strcat(makeadir, newdir);
     strcat(makeadir, cndir);
@@ -21,13 +26,149 @@ void Createdir(){
     }
 
 void CreateFile(){
-    char makeafile[256];
-    char newfile[256];
+    char makeafile[256]= "";
+    char newdir[10]= "/";
+    char newfile[256]="";
     strcat(makeafile, commands[1]);
     strcat(makeafile, combinedDir);
-    scanw("/%s", &newfile);
+    strcat(makeafile, newdir);
+    mvprintw(1, (x/4+1), "New File Name: ");
+    echo();
+    getstr(newfile);
+    noecho();
     strcat(makeafile, newfile);
     system(makeafile);
+}
+
+void Copy(){
+ccopy=TRUE;
+ccut=FALSE;
+char newdir[10]= "/";
+
+memset(copyfrom, 0, sizeof(copyfrom));
+ char *newline = strchr(copyfrom, '\n');
+    if (newline) *newline = '\0';
+strcat(copyfrom, combinedDir);
+strcat(copyfrom, newdir);
+strcat(copyfrom, options[invertedcordinates]);
+
+}
+
+void Cut(){
+ccopy=FALSE;
+ccut=TRUE;
+char newdir[10]= "/";
+
+memset(copyfrom, 0, sizeof(copyfrom));
+ char *newline = strchr(copyfrom, '\n');
+    if (newline) *newline = '\0';
+strcat(copyfrom, combinedDir);
+strcat(copyfrom, newdir);
+strcat(copyfrom, options[invertedcordinates]);
+}
+
+void Paste(){
+if(ccopy==TRUE){
+char copying[256]="";
+char space[8]= " ";
+
+ char *newline = strchr(copying, '\n');
+    if (newline) *newline = '\0';
+strcat(copying, commands[2]);
+strcat(copying, copyfrom);
+strcat(copying, space);
+strcat(copying, combinedDir);
+
+system(copying);
+
+memset(copying, 0, sizeof(copying));
+
+}
+
+if(ccut==TRUE){
+char copying[256];
+char space[8]= " ";
+
+strcat(copying, commands[3]);
+strcat(copying, copyfrom);
+strcat(copying, space);
+strcat(copying, combinedDir);
+system(copying);
+memset(copying, 0, sizeof(copying));
+}
+
+
+}
+
+void Rename(){
+
+char newdir[10]= "/";
+char renaming[256]= "";
+char cding[256];
+char rename[256];
+char space[8]= " ";
+char cd[8]= "cd ";
+char joint[562];
+char ampersons[8]= " && ";
+memset(renaming, 0, sizeof(renaming));
+memset(joint, 0, sizeof(joint));
+memset(cding, 0, sizeof(cding));
+ char *newline = strchr(renaming, '\n');
+    if (newline) *newline = '\0';
+strcat(renaming, commands[5]);
+strcat(cding, cd);
+strcat(cding, combinedDir);
+strcat(renaming, options[invertedcordinates]);
+ mvprintw(1, (x/4+1), "Rename To: ");
+echo();
+getstr(rename);
+noecho();
+strcat(renaming, space);
+strcat(renaming, rename);
+strcat(joint, cding);
+strcat(joint, ampersons);
+strcat(joint, renaming);
+system(joint);
+}
+
+void Execute(){
+char cd[8]= "cd ";
+char cding[256];
+char exec[256]= "";
+char joint[500];
+char ampersons[8]= " && ";
+
+memset(cding, 0, sizeof(cding));
+memset(exec, 0, sizeof(exec));
+memset(joint, 0, sizeof(joint));
+
+strcat(cding, cd);
+strcat(cding, combinedDir);
+
+strcat(exec, commands[6]);
+strcat(exec, options[invertedcordinates]);
+
+strcat(joint, cding);
+strcat(joint, ampersons);
+strcat(joint, exec);
+
+system(joint);
+}
+
+void Wine(){
+char cd[8]= "cd ";
+char cding[256];
+char wine[256]= "";
+char newdir[10]= "/";
+
+memset(cding, 0, sizeof(cding));
+strcat(cding, cd);
+strcat(cding, combinedDir);
+
+strcat(wine, commands[7]);
+strcat(wine, options[invertedcordinates]);
+
+system(wine);
 }
 
 void Actioncall(){
@@ -40,8 +181,34 @@ if(commandcall==TRUE){
         case 1:
         CreateFile();
         break;
+
+        case 2: 
+        Copy();
+        break;
+
+        case 3: 
+        Cut();
+        break;
+
+        case 4:
+        Paste();
+        break;
+        
+        case 5:
+        Rename();
+        break;
+ 
+        case 6:
+        Execute();
+        break;
+
+         case 7:
+        Wine();
+        break;
     }
+
     commandcall=FALSE;
 }
 }
+
 #endif // !ACTIONSC
